cd779e99f525b7cc38687777659eb28349bd6568
Alex Sánchez Pla <asanchez@ub.edu>
2021-07-05 23:00:52 +0200

    lab for the workshop is working now

---
+++ InstallPackages.R
@@ -0,0 +1,39 @@
+if(!require(BiocManager)){
+  install.packages("BiocManager", dep=TRUE)
+}
+
+installifnot <- function (pckgName, BioC=TRUE){
+  if(BioC){
+    if(!require(pckgName, character.only=TRUE, quietly = TRUE)){
+      BiocManager::install(pckgName)
+    }
+  }else{
+    if(!require(pckgName, character.only=TRUE, quietly=TRUE)){
+      install.packages(pckgName, dep=TRUE)
+    }
+  }
+}
+installifnot("Biobase")
+installifnot("NMF", BioC=FALSE)
+installifnot("devtools", BioC=FALSE)
+installifnot("knitr", BioC=FALSE)
+installifnot("kableExtra", BioC=FALSE)
+installifnot("ggplot2", BioC=FALSE)
+installifnot("dplyr", BioC=FALSE)
+installifnot("fastICA", BioC=FALSE)
+installifnot("Rtsne", BioC=FALSE)
+
+installifnot("matrixStats", BioC=FALSE)
+installifnot("FactoMineR", BioC=FALSE)
+
+installifnot("org.Hs.eg.db")
+installifnot("SummarizedExperiment")
+installifnot("pheatmap")
+installifnot("iClusterPlus")
+
+installifnot("enrichR")
+
+devtools::install_github("compgenomr/compGenomRData")
+# If this does not work try to clone/download the repo into you computer
+# Once you have it in your disk install it using:
+# devtools::install_local("PATH_TO_compGenomRData")
